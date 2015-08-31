fb8634f17155a2cd73f818c898cfca3dcf9bfd58
unknown <gustavobrittogebrim@gmail.com>
2015-08-31 17:12:28 -0300

    Release v0.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ var  gulp = require('gulp'),
      plumber = require('gulp-plumber'),
      qunit = require('gulp-qunit'),
      pkg = require('./package.json'),
+     rename = require('gulp-rename'),
+     clean = require('gulp-clean'),
+     bump = require('gulp-bump'),
+     minifyCss = require('gulp-minify-css'),
+     git = require('gulp-git'),
      uglify = require('gulp-uglify');
 
 
@@ -18,21 +23,52 @@ gulp.task('serve', function() {
             baseDir: "./src"
         }
     });
-
     gulp.watch("src/styles/*.scss", ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
+gulp.task('clean', function () {
+    return gulp.src('./dist', { read: false })
+        .pipe(clean());
+});
 
-gulp.task('build', function() {
-    return gulp.src('./src/knockout-validate.js')
+gulp.task('build:js', function() {
+    return gulp.src('./src/scripts/knockout-validate.js')
         .pipe(gulp.dest('./dist'))
         .pipe(uglify())
+        .pipe(rename('knockout-validate.min.js'))
+        .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('release', function() {
-
+gulp.task('build:css', function() {
+    return gulp.src('./src/styles/knockout-validate.css')
+        .pipe(gulp.dest('dist'))
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(rename('knockout-validate.min.css'))
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('build', ['build:css', 'build:js']);
+
+gulp.task('bump', ['build'], function () {
+    return gulp.src(['./package.json', './bower.json'])
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('tag', ['bump'], function () {
+    var pkg = require('./package.json');
+    var v = 'v' + pkg.version;
+    var message = 'Release ' + v;
+
+    return gulp.src('./')
+        .pipe(git.commit(message))
+        .pipe(git.tag(v, message))
+        .pipe(git.push('origin', 'master', '--tags'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('release', ['tag']);
 
 // test
 gulp.task('test', function() {
