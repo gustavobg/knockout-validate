415536b9d0b0b815f0ab59e2458079276a90a12f
gustavobg <gustavobrittogebrim@gmail.com>
2015-10-09 11:22:48 -0300

    fixed knockout require name

--- src/scripts/knockout-validate.js
+++ src/scripts/knockout-validate.js
@@ -1,10 +1,10 @@
 ﻿(function (factory) {
     if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
         // CommonJS or Node: hard-coded dependency on "knockout"
-        factory(require("jquery"), require("ko"), exports);
+        factory(require("jquery"), require("knockout"), exports);
     } else if (typeof define === "function" && define["amd"]) {
         // AMD anonymous module with hard-coded dependency on "knockout"
-        define(["jquery", "ko", "exports"], factory);
+        define(["jquery", "knockout", "exports"], factory);
     } else {
         // <script> tag: use the global `ko` object, attaching a `mapping` property
         factory(jQuery, ko, ko.validate = {});
